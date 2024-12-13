cd88d90759bf9404fd33bdd4dce7460a8aef885d
Ravil <ravil.shainidze12@gmail.com>
2024-12-12 20:02:05 -0600

    minor styling change: quotations from single to double (in 2 files)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,13 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import LoginForm from '@/app/ui/login-form';
-import { Metadata } from 'next';
+import AcmeLogo from "@/app/ui/acme-logo";
+import LoginForm from "@/app/ui/login-form";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Login',
+  title: "Login",
 };
 
 export default function LoginPage() {
+  console.log("AUTH_URL:", process.env.AUTH_URL);
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
